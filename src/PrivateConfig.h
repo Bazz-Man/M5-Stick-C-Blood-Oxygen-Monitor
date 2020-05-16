8bdc0e027091ce2c404cbd8e663f4c7f932e8842
Bazz Man <33786321+Bazz-Man@users.noreply.github.com>
2020-05-16 14:33:27 +0100

    First Working Version

--- src/PrivateConfig.h
+++ src/PrivateConfig.h
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#define NETWORKNAME1 "network1"
-#define NETWORKPASS1 "network1 password"
-#define NETWORKNAME2 "network2"
-#define NETWORKPASS2 "network2 password"
-#define NETWORKNAME3 "network3"
-#define NETWORKPASS3 "network3 password"
-#define NETWORKNAME4 "network4"
-#define NETWORKPASS4 "network4 password "
-
-/** START - MQTT parameters **/
-char* MQTTOutputTopic = "Monitor/OUT";
-char* MQTTOutputDiag = "Monitor/DIAG";
-char* MQTTInputTopic = "Monitor/IN";
-const char* MQTT_SERVER_CentralHub = "CentralHub";
-//const char* MQTT_SERVER_CentralHub = "192.168.X.X";
-/** END - MQTT parameters **/
-=======
 #define NETWORKNAME1 "network1"
 #define NETWORKPASS1 "network1 password"
 #define NETWORKNAME2 "network2"
@@ -32,4 +14,3 @@ char* MQTTInputTopic = "Monitor/IN";
 const char* MQTT_SERVER_CentralHub = "CentralHub";
 //const char* MQTT_SERVER_CentralHub = "192.168.X.X";
 /** END - MQTT parameters **/
->>>>>>> 689a7a78b29c740283f0f7038e3825854e87b8f9
